94a21ea99ee4772a5833b8318c5ae8af329770a2
Marc Grossi <tecnogrossi@gmail.com>
2024-06-07 20:30:45 +0200

    finish section 20

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lifecycle');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lifecycle app is running!');
   });
 });
